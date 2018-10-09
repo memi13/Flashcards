f869d292a79f8827c51400ce9c1a1aa4438ac42d
linus <linus@10.32.104.44>
2018-10-09 09:28:24 +0200

    3333

---
+++ Flashcards/src/model/LanguageBox.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author linus
+ *
+ */
+public class LanguageBox implements IDBFunctions {
+
+	
+	
+	/**
+	 * test
+	 */
+	public LanguageBox() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see model.IDBFunctions#save()
+	 */
+	@Override
+	public boolean save() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see model.IDBFunctions#delete()
+	 */
+	@Override
+	public boolean delete() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ Flashcards/src/model/User.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author linus
+ *
+ */
+public class User implements IDBFunctions {
+
+	/**
+	 * test
+	 */
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see model.IDBFunctions#save()
+	 */
+	@Override
+	public boolean save() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see model.IDBFunctions#delete()
+	 */
+	@Override
+	public boolean delete() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
