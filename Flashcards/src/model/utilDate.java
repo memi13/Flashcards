5e25ac243178d5d8aa1624e9193b3260aca657b7
linus <linus@10.32.104.44>
2018-10-09 12:54:00 +0200

    utildate

---
+++ Flashcards/src/model/utilDate.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.util.Date;
+
+public class utilDate extends Date {
+
+	public utilDate() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public utilDate(long arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+}
