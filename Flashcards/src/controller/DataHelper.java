1e0d5af62d35baaca793f9c501870571950b2f59
Michael <Michael@DESKTOP-NB8RTQU>
2018-10-09 13:08:50 +0200

    fdg

--- Flashcards/src/controller/DataHelper.java
+++ Flashcards/src/controller/DataHelper.java
@@ -5,52 +5,72 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
-public class DataHelper {
+import model.IDBFunctions;
+
+public class DataHelper implements IDataHelper{
 
 	public DataHelper() {
 		// TODO Auto-generated constructor stub
 	}
+
+	@Override
+	public boolean checkLogin(String user, String pw) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ArrayList<IDBFunctions> getLanguageBoxes(int idUser) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<IDBFunctions> getCards(int languageBox, int comp) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean moveCard(int correct) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ArrayList<IDBFunctions> getStatisticData(int idUser) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean updateCard(IDBFunctions card) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteCard(IDBFunctions card) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean newLanguageBox(String name) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean saveSetting(IDBFunctions user) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 	
-	public static void connect() {
-        Connection conn = null;
-        try {
-            // db parameters asdfasdfasdf
-            String url = "jdbc:sqlite:C:/sqlite/db/CardDB.db";
-            //
-            // create a connection to the database
-            conn = DriverManager.getConnection(url);
-            
-            String sql = "SELECT id, sText FROM Card";
-            
-            try (
-                 Statement stmt  = conn.createStatement();
-                 ResultSet rs    = stmt.executeQuery(sql)){
-                
-                // loop through the result set
-                while (rs.next()) {
-                    System.out.println(rs.getInt("id") +  "\t" + 
-                                       rs.getString("sText"));
-                }
-            } catch (SQLException e) {
-                System.out.println(e.getMessage());
-            }
-            
-            System.out.println("Connection to SQLite has been established.");
-            
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            try {
-                if (conn != null) {
-                	conn.close();
-                	System.out.println("Closed");
-                }
-        	}catch (SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
-        }
-    }
+	
+    
 	
 	
 	
