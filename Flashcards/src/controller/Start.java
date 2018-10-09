c1413fc410a4490fea1b5ed9931c4f16d3169898
linus <linus@10.32.104.44>
2018-10-09 12:58:12 +0200

    asd

--- Flashcards/src/controller/Start.java
+++ Flashcards/src/controller/Start.java
@@ -1,11 +1,15 @@
 package controller;
 
+import model.Card;
+
 public class Start {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		DataHelper d = new DataHelper();
-		d.connect();
+		Card c = new Card(3);
+		c.setCountWrong(99);
+		c.setsText("22CREATEDinclSSTatistik");
+		c.createRecord();
 	}
 
 }
